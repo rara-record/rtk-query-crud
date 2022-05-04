b3b815a2320724c673962af0b37b82b7a3492c05
kimbora <a52447879@gmail.com>
2022-05-05 00:13:27 +0900

    [feat] Delete 로직 추가

--- src/store/api/contactsApi.ts
+++ src/store/api/contactsApi.ts
@@ -4,18 +4,36 @@ import { IContact } from "../../models/contact.model";
 export const contactsApi = createApi({
   reducerPath: "contactsApi",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
+  tagTypes: ["Contact"],
   endpoints: (builder) => ({
     fetchContacts: builder.query<IContact[], void>({
       query: () => "/contacts",
+      providesTags: ["Contact"],
     }),
-    addContacts: builder.mutation<{}, IContact>({
+    fetchContactById: builder.query<IContact, string>({
+      query: (id) => `/contacts/${id}`,
+    }),
+    addContact: builder.mutation<{}, IContact>({
       query: (contact) => ({
         url: "/contacts",
         method: "POST",
         body: contact,
       }),
+      invalidatesTags: ["Contact"],
+    }),
+    deleteContact: builder.mutation<void, string>({
+      query: (id) => ({
+        url: `/contacts/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Contact"],
     }),
   }),
 });
 
-export const { useFetchContactsQuery, useAddContactsMutation } = contactsApi;
+export const {
+  useFetchContactsQuery,
+  useFetchContactByIdQuery,
+  useAddContactMutation,
+  useDeleteContactMutation,
+} = contactsApi;
